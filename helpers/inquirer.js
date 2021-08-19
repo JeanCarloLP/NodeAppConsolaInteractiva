80b4bb1b559708d119e6c1aae4986ab3f3345278
Jean Carlo Leon <jeancarloleonpr@gmail.com>
2021-08-19 17:25:28 +0200

    19082021_Update

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,35 +1,68 @@
 require('colors');
 
 const inquirer = require('inquirer');
-
 const preguntas = [
     {
         type: 'list',
         name: 'opcion',
         message: '¿Qué opción desea seleccionar?',
-        choices: ['opt1','opt2','opt3','opt4','opt5','opt6','opt0']
+        choices: [
+            {
+                value: '1',
+                name: '1.- Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2.- Listar tarea'
+            },
+            {
+                value: '3',
+                name: '3.- Listar tareas completadas'
+            },
+            {
+                value: '4',
+                name: '4.- Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5.- Completar Tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6.- Borrar Tarea'
+            },
+            {
+                value: '0',
+                name: '0.- Salir'
+            }
+        ]
     }
 ];
+
 const inquirerMenu = async() => {
-    //console.clear();
+    console.clear();
     console.log('================================'.green);
     console.log('     Seleccione una opción'.green);
     console.log('================================\n'.green);
 
-
-    const opt = await inquirer.prompt( preguntas );
-
-    return opt;
-    // console.log(`${ '1.'.green } Crear tarea`);
-    // console.log(`${ '2.'.green } Listar tareas`);
-    // console.log(`${ '3.'.green } Listar tareas completadas`);
-    // console.log(`${ '4.'.green } Listar tareas pendientes`);
-    // console.log(`${ '5.'.green } Completar Tarea(s)`);
-    // console.log(`${ '6.'.green } Borrar Tarea`);
-    // console.log(`${ '0.'.green } Salir\n`);
-
+    const { opcion } = await inquirer.prompt( preguntas );
+    return opcion;
 }
 
+const pausa = async() => {
+    const question = [
+        {
+            type: 'input',
+            name: 'pausa',
+            message: `Presione ${ 'ENTER'.green } para continuar\n`
+        }
+    ];
+
+    console.log('\n')
+    await inquirer.prompt( question );
+};
+
 module.exports = {
-    inquirerMenu
+    inquirerMenu,
+    pausa
 }
---
+++ models/tareas.js
@@ -0,0 +1,18 @@
+// Ejemplo de listado para identificarlo en un Objeto
+/**
+ *  _listado: {
+ *      'uuid-128-128128123-123123: { id:2, desc: asdasd, completadoEn: 238238 },
+ *      'uuid-128-128128123-386386: { id:3, desc: a2dasd, completadoEn: 338238 }
+ *  }
+ */
+
+
+class Tareas {
+    _listado = {};
+
+    constructor() {
+        this._listado = {};
+    }
+}
+
+module.exports = Tareas;
